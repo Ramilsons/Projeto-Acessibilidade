d64dfbcf45894033780e22fa4d8fac0f2e96eab2
Ramilsons <ramilsonfelixsilva9@gmail.com>
2021-10-07 20:02:35 -0300

    Primeiro commit

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<footer>
+    <nav>
+        <ul>Navegue pelo nosso site
+            <li><a href="">Página 1</a></li>
+            <li><a href="">Página 2</a></li>
+            <li><a href="">Página 3</a></li>
+            <li><a href="">Página 4</a></li>
+            <li><a href="">Página 5</a></li>
+            <li><a href="">Página 6</a></li>
+            <li><a href="">Página 7</a></li>
+            <li><a href="">Página 8</a></li>
+            <li><a href="">Página 9</a></li>
+        </ul>
+    </nav>
+    <p>Lorem ipsum dolor sit amet.</p>
+</footer>
