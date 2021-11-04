449815ba0beab510df050afe2b32a6bc64770f8f
VitorHugoTeixeira <vitor4oliveira@gmail.com>
2021-11-04 17:11:45 -0300

    Adicionando tags do Bootstrap no código da Página

--- principais_falhas.php
+++ principais_falhas.php
@@ -11,41 +11,48 @@
     <?php include './includes/header.php'; ?>
     <main>
         <article>
-            <div class="row">
-                <div class="col-6">
-                    <img src="" alt="..." class="img-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <img src="" alt="..." class="img-fluid">
+                    </div>
+                    <div class="col-6">
+                        <h1>Principais Falhas</h1>
+                        <h3>--Data correspondente--</h3>
+                    </div>
                 </div>
-                <div class="col-6">
-                    <h1>Principais Falhas</h1>
-                    <h3>--Data correspondente--</h3>
+
+                <div class="row">
+                    <div class="col-6">
+                        <h2>2.Título</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elit eros, pretium a elementum
+                            vitae, scelerisque ac augue. Curabitur at euismod eros. Vestibulum diam libero, posuere non
+                            luctus commodo, tincidunt nec lectus. Integer consectetur ipsum vel nunc fringilla, vel
+                            pulvinar est ullamcorper. Proin in nibh et mi accumsan imperdiet. Pellentesque et libero
+                            auctor, tempus arcu eget, venenatis massa. Phasellus eget lectus enim. Etiam dui lectus,
+                            tincidunt at sem ac, semper dapibus sapien. Sed dolor quam, rutrum in vestibulum non,
+                            ullamcorper quis sem. Integer ligula orci, convallis et placerat at, posuere nec dolor.
+                            Pellentesque porta gravida tortor non eleifend. Aenean gravida nisi vel tempus rhoncus.
+                            Proin orci justo, luctus vel nisl eget, porttitor vestibulum magna. Aliquam consectetur
+                            fringilla varius. Aliquam sed fringilla sem.</p>
+
+                    </div>
+                    <hr>
+                    <div class="col-6">
+                        <h2>1.Título</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elit eros, pretium a elementum
+                            vitae, scelerisque ac augue. Curabitur at euismod eros. Vestibulum diam libero, posuere non
+                            luctus commodo, tincidunt nec lectus. Integer consectetur ipsum vel nunc fringilla, vel
+                            pulvinar est ullamcorper. Proin in nibh et mi accumsan imperdiet. Pellentesque et libero
+                            auctor, tempus arcu eget, venenatis massa. Phasellus eget lectus enim. Etiam dui lectus,
+                            tincidunt at sem ac, semper dapibus sapien. Sed dolor quam, rutrum in vestibulum non,
+                            ullamcorper quis sem. Integer ligula orci, convallis et placerat at, posuere nec dolor.
+                            Pellentesque porta gravida tortor non eleifend. Aenean gravida nisi vel tempus rhoncus.
+                            Proin orci justo, luctus vel nisl eget, porttitor vestibulum magna. Aliquam consectetur
+                            fringilla varius. Aliquam sed fringilla sem.</p>
+                    </div>
                 </div>
             </div>
-
-            <h2>1.Título</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elit eros, pretium a elementum
-                vitae, scelerisque ac augue. Curabitur at euismod eros. Vestibulum diam libero, posuere non
-                luctus commodo, tincidunt nec lectus. Integer consectetur ipsum vel nunc fringilla, vel
-                pulvinar est ullamcorper. Proin in nibh et mi accumsan imperdiet. Pellentesque et libero
-                auctor, tempus arcu eget, venenatis massa. Phasellus eget lectus enim. Etiam dui lectus,
-                tincidunt at sem ac, semper dapibus sapien. Sed dolor quam, rutrum in vestibulum non,
-                ullamcorper quis sem. Integer ligula orci, convallis et placerat at, posuere nec dolor.
-                Pellentesque porta gravida tortor non eleifend. Aenean gravida nisi vel tempus rhoncus.
-                Proin orci justo, luctus vel nisl eget, porttitor vestibulum magna. Aliquam consectetur
-                fringilla varius. Aliquam sed fringilla sem.</p>
-
-            <hr>
-
-            <h2>2.Título</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elit eros, pretium a elementum
-                vitae, scelerisque ac augue. Curabitur at euismod eros. Vestibulum diam libero, posuere non
-                luctus commodo, tincidunt nec lectus. Integer consectetur ipsum vel nunc fringilla, vel
-                pulvinar est ullamcorper. Proin in nibh et mi accumsan imperdiet. Pellentesque et libero
-                auctor, tempus arcu eget, venenatis massa. Phasellus eget lectus enim. Etiam dui lectus,
-                tincidunt at sem ac, semper dapibus sapien. Sed dolor quam, rutrum in vestibulum non,
-                ullamcorper quis sem. Integer ligula orci, convallis et placerat at, posuere nec dolor.
-                Pellentesque porta gravida tortor non eleifend. Aenean gravida nisi vel tempus rhoncus.
-                Proin orci justo, luctus vel nisl eget, porttitor vestibulum magna. Aliquam consectetur
-                fringilla varius. Aliquam sed fringilla sem.</p>
         </article>
     </main>
 
