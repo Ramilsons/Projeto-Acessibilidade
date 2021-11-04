fb4e3638f804cbd80608ec58a5a006aeeb22d060
tatiananm <mishimatatiana@gmail.com>
2021-11-04 19:31:10 -0300

    alteração imagem.php

--- imagem.php
+++ imagem.php
@@ -21,22 +21,46 @@
         </section>
         <article>            
         <p>
+        <div class="d-grid gap-2 col-6 mx-auto">
   <a class="btn btn-dark " data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Tópico 1</a>
   <button class="btn btn-dark " type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Tópico 2</button>
-  <button class="btn btn-dark " type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">Fechar</button>
+  <button class="btn btn-dark " type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample3" aria-expanded="false" aria-controls="multiCollapseExample3">Tópico 3</button>
 </p>
 <div class="row">
+<div class="text-center">
   <div class="col ">
     <div class="collapse multi-collapse" id="multiCollapseExample1">
       <div class="card card-body">
-        Some placeholder content for the first collapse component of this multi-collapse example. This panel is hidden by default but revealed when the user activates the relevant trigger.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elit eros, pretium a elementum
+            vitae, scelerisque ac augue. Curabitur at euismod eros. Vestibulum diam libero, posuere non
+            luctus commodo, tincidunt nec lectus. Integer consectetur ipsum vel nunc fringilla, vel
+            pulvinar est ullamcorper. Proin in nibh et mi accumsan imperdiet. Pellentesque et libero
+            auctor, tempus arcu eget, venenatis massa. Phasellus eget lectus enim. Etiam dui lectus,
+            tincidunt at sem ac, semper dapibus sapien.
       </div>
     </div>
   </div>
   <div class="col ">
     <div class="collapse multi-collapse" id="multiCollapseExample2">
       <div class="card card-body">
-        Some placeholder content for the second collapse component of this multi-collapse example. This panel is hidden by default but revealed when the user activates the relevant trigger.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elit eros, pretium a elementum
+            vitae, scelerisque ac augue. Curabitur at euismod eros. Vestibulum diam libero, posuere non
+            luctus commodo, tincidunt nec lectus. Integer consectetur ipsum vel nunc fringilla, vel
+            pulvinar est ullamcorper. Proin in nibh et mi accumsan imperdiet. Pellentesque et libero
+            auctor, tempus arcu eget, venenatis massa. Phasellus eget lectus enim. Etiam dui lectus,
+            tincidunt at sem ac, semper dapibus sapien.
+      </div>
+    </div>
+  </div>
+  <div class="col ">
+    <div class="collapse multi-collapse" id="multiCollapseExample3">
+      <div class="card card-body">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elit eros, pretium a elementum
+            vitae, scelerisque ac augue. Curabitur at euismod eros. Vestibulum diam libero, posuere non
+            luctus commodo, tincidunt nec lectus. Integer consectetur ipsum vel nunc fringilla, vel
+            pulvinar est ullamcorper. Proin in nibh et mi accumsan imperdiet. Pellentesque et libero
+            auctor, tempus arcu eget, venenatis massa. Phasellus eget lectus enim. Etiam dui lectus,
+            tincidunt at sem ac, semper dapibus sapien.
       </div>
     </div>
   </div>
