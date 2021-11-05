4abfaf689180b0877a236e9e3ce5bd546486a32e
VitorHugoTeixeira <vitor4oliveira@gmail.com>
2021-11-04 21:01:51 -0300

    Ajustando toogle button do menu

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,35 @@
     <!-- Esse link deverá aparecer quando o usuário apertar tab pela primeira vez na página-->
     <a href="" class="visually-hidden-focusable">Ir para o conteúdo principal</a>
     
-    <!-- Botão de menu aparecerá apenas para tablet e mobile -->
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Exibir Menu">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <!-- Botão de menu aparecerá apenas para tablet e mobile -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <img src="" alt="Menu">
+            </button>
+            <div class="collapse navbar-collapse justify-content-lg-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item ps-lg-3">
+                        <a class="nav-link active" href="#">Página 1</a>
+                    </li>
+                    <li class="nav-item ps-lg-3">
+                        <a class="nav-link" href="#">Página 2</a>
+                    </li>
+                    <li class="nav-item ps-lg-3">
+                        <a class="nav-link" href="#">Página 3</a>
+                    </li>
+                    <li class="nav-item ps-lg-3">
+                        <a class="nav-link" href="#">Página 4</a>
+                    </li>
+                    <li class="nav-item ps-lg-3">
+                        <a class="nav-link" href="#">Página 5</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Exibir Menu">
         <img src="" alt="Menu">
     </button>
 
@@ -15,5 +42,5 @@
             <li class="nav-item col-12 col-sm-2"><a class="nav-link" href="contato.php">Página 4</a></li>
             <li class="nav-item col-12 col-sm-2"><a class="nav-link" href="menu.php">Página 5</a></li>
         </ul>
-    </nav>
+    </nav> -->
 </header>
