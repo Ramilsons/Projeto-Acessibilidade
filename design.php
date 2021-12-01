cf9ce8093f24036f1deb3d1ff8a66b8e9eb7819d
VitorHugoTeixeira <vitor4oliveira@gmail.com>
2021-12-01 17:03:49 -0300

    Estruturando página de Design para apresentação

--- design.php
+++ design.php
@@ -12,45 +12,42 @@
     <main>
         <?php include './includes/header.php'; ?> <main>
             <article>
-                <div class="container justify-content-center">
-                    <div class="row">
+                <div class="container">
+                    <div class="row mt-5 mb-5">
                         <div class="col-6" id="div-linha">
                             <h3 class="titulo_forma">W3C e o Design Acessível</h3>
-                            <!-- <div class="div_imagem_forma">
-                                <img src="..." width="300px" height="300px" alt="..." class="img-fluid">
-                            </div> -->
+
+                            <img src="img/design/design1.jpg" alt="" class="img-fluid" id="img_design1">
 
                             <h4 class="subtitulo">A importância do Design Acessível</h4>
-                            <p>O design é projeção das ideias dos desenvolvedores para a visualização dos usuários, podendo tornar a utilização da aplicação mais simples, facilitando a vida do usuário. Quando pensamos em acessibilidade, o que nos vem a mente em primeiro lugar é a inclusão. O fato que no mundo temos diversas pessoas que possuem algum tipo de deficiência que dificulta tarefas diárias como navegar na internet. Isso nos leva a buscar diversas formas de tornar nossa aplicação mais inclusiva e acessível para todas as pessoas do mundo. </p>
+                            <p class="text_subtitulo">O design é a projeção das ideias dos desenvolvedores para a visualização dos usuários, podendo tornar a utilização da aplicação mais simples, facilitando a vida do usuário. Quando pensamos em acessibilidade, o que nos vem a mente em primeiro lugar é a inclusão. O fato que no mundo temos diversas pessoas que possuem algum tipo de deficiência que dificulta tarefas diárias como navegar na internet. Isso nos leva a buscar diversas formas de tornar nossa aplicação mais inclusiva e acessível para todas as pessoas do mundo. </p>
                             <div class="row explicacao">
-                                <p>Com isso em mente, devemos seguir algumas diretrizes para tornar nossos projetos visualmente mais acessíveis. A W3C, uma organização que visa a inclusão de todas as pessoas na WEB, criou diversos padrões a serem seguidos para um design acessível. Clique aqui para descobrir.” https://www.w3c.br/Padroes/WebDesignAplicacoes” </p>
+                                <p>Com isso em mente, devemos seguir algumas diretrizes para tornar nossos projetos visualmente mais acessíveis. A W3C, uma organização que visa a inclusão de todas as pessoas na WEB, criou diversos padrões a serem seguidos para um design acessível.</p>
+                                <a href="https://www.w3c.br/Padroes/WebDesignAplicacoes">Clique aqui para acessar o design de acordo com a W3C</a>
                             </div>
 
-                            <img src="" alt="" c>
+                            <img src="img/design/design3.jpg" alt="" class="img-fluid" id="img_design2">
                         </div>
-                        
-                        <div class="col-6">
-                            <h3 class="titulo_forma">Como fazer um design acessivel</h3>
-                            <div class="div_imagem_forma">
-                                <!-- <img src="img/adesivo-paisagem-praia-decorando-com-adesivos.jpg" width="300px" height="300px" alt="..." class="img-fluid"> -->
+
+                        <div class="col-6" id="div-linha2">
+                            <h3 class="titulo_forma2">Como fazer um design acessivel</h3>
+
+                            <img src="img/design/design2.jpg" alt="" class="img-fluid" id="img_design2">
+
+                            <h4 class="subtitulo2">Dicas de Design Acessivel</h4>
+                            <div class="bloco_texto">
+                                <h5>Contraste de cor acessível</h5>
+                                <p>Escolhendo a paleta de cores da sua página ou aplicação, faça testes relacionados à cor do texto versus cor de fundo da página, beneficiando pessoas com daltonismo</p>
+                                <h5>Design Responsivo</h5>
+                                <p>O design responsivo tem uma relação direta com a acessibilidade. Fazer com que o software se adapte à tela do usuário é importantíssimo, bem como evitar bloqueio de orientação e zoom. A forma como o conteúdo vai ser exibido, o tamanho das fontes, botões e contraste de cor trabalham em conjunto para garantir que o design responsivo seja acessível.</p>
                             </div>
-                            <h4 class="subtitulo">Dicas de Design Acessivel</h4>
-                            <h5>Contraste de cor acessível</h5>
-                            <p>Escolhendo a paleta de cores da sua página ou aplicação, faça testes relacionados
-                                à cor do texto versus cor de fundo da página,
-                                beneficiando pessoas com daltonismo</p>
-                            <h5>Tamanho do Texto</h5>
-                            <p>Um tamanho “mínimo” é algo entre 14px, desde que as questões relacionadas ao contraste sejam rigorosamente respeitadas. Entretanto, o usuário pode aumentar o tamanho de fontes, ele também pode diminuir pelo próprio navegador, se assim desejar.</p>
-                            <h5>Design Responsivo</h5>
-                            <p>O design responsivo tem uma relação direta com a acessibilidade. Fazer com que o software se adapte à tela do usuário é importantíssimo, bem como evitar bloqueio de orientação e zoom. A forma como o conteúdo vai ser exibido, o tamanho das fontes, botões e contraste de cor trabalham em conjunto para garantir que o design responsivo seja acessível.</p>
-                            <p></p>
 
 
-                            <div class="row explicacao">
+                            <div class="row explicacao2">
                                 <p>Essas são regras que devem ser seguidas a risca, é muito importante prestar atenção nelas para projetar um site de forma acessível para todos os usuários da web. A W3C também é uma forma de consulta primordial quando mencionamos acessibilidade na web. </p>
                             </div>
 
-                            <img src="" alt="" c>
+                            <img src="img/design/design4.jpg" alt="" class="img-fluid" id="img_design2">
                         </div>
                     </div>
                 </div>
