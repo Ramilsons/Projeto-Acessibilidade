645286ab2145b4e8599af9ce91d2c3a2dbed8a22
tatiananm <mishimatatiana@gmail.com>
2021-12-02 14:56:09 -0300

    adicao de texto pagina imagem e tabela

--- imagem.php
+++ imagem.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <?php include './includes/bootstrap-css.php'; ?>
+    <link rel="stylesheet" href="./css/paginas/menu.css">
    
 <title>Imagem | Seu site mais acessível</title>
 
@@ -16,58 +17,173 @@
            <div class="jumbotron jumbotron-fluid">
             <div class="container text-center">
               <h1 class="display-4">Trabalhando com Imagens </h1>
-            </div>
+              </div>
+              </div>
+              <div class="container text-left">
+              <h2 class="título secundário">As imagens devem ter alternativas de texto que descrevam as informações ou funções representadas por elas. Isso garante que as imagens possam ser usadas por pessoas com várias deficiências. Este tutorial demonstra como fornecer alternativas de texto apropriadas com base na finalidade da imagem:</h2>
           </div>  
         </section>
         <article>            
         <p>
         <div class="d-grid gap-2 col-6 mx-auto">
-  <a class="btn btn-dark " data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Tópico 1</a>
-  <button class="btn btn-dark " type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Tópico 2</button>
-  <button class="btn btn-dark " type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample3" aria-expanded="false" aria-controls="multiCollapseExample3">Tópico 3</button>
+  <a class="btn btn-dark " data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Imagens Informativas</a>
+  <button class="btn btn-dark " type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Imagens Decorativas</button>
+  <button class="btn btn-dark " type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample3" aria-expanded="false" aria-controls="multiCollapseExample3">Imagens Funcionais</button>
+  <button class="btn btn-dark " type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample4" aria-expanded="false" aria-controls="multiCollapseExample4">Imagens Complexas</button>
 </p>
-<div class="row">
-<div class="text-center">
   <div class="col ">
     <div class="collapse multi-collapse" id="multiCollapseExample1">
       <div class="card card-body">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elit eros, pretium a elementum
-            vitae, scelerisque ac augue. Curabitur at euismod eros. Vestibulum diam libero, posuere non
-            luctus commodo, tincidunt nec lectus. Integer consectetur ipsum vel nunc fringilla, vel
-            pulvinar est ullamcorper. Proin in nibh et mi accumsan imperdiet. Pellentesque et libero
-            auctor, tempus arcu eget, venenatis massa. Phasellus eget lectus enim. Etiam dui lectus,
-            tincidunt at sem ac, semper dapibus sapien.
-      </div>
+     <p> Imagens que representam graficamente conceitos e informações, normalmente imagens, fotos e ilustrações. A alternativa em texto deve ser pelo menos uma breve descrição que transmita as informações essenciais apresentadas pela imagem.
+As imagens informativas transmitem um conceito ou informação simples que pode ser expressa em uma frase ou frase curta. A alternativa em texto deve transmitir o significado ou conteúdo exibido visualmente, o que normalmente não é uma descrição literal da imagem.
+Em algumas situações, uma descrição literal detalhada pode ser necessária, mas apenas quando o conteúdo da imagem é toda ou parte das informações transmitidas. Tratar uma imagem como informativa ou decorativa é um julgamento que os autores fazem, com base no motivo da inclusão da imagem na página.
+Exemplo 01:<p/>
+<div class="row mt-5 mb-5" id="imgs_tampa"> 
+<div class="col-12">
+<img src="./img/tampa.jpg" class="img-fluid" alt="Empurre a tampa para baixo e gire no sentido anti-horário (da direita para a esquerda)" id="img01"/>
+                    </div>
+                    </div>
+  <p> Este diagrama simples ilustra uma direção anti-horária para desparafusar a tampa ou tampa de uma garrafa. As informações podem ser descritas em uma frase curta, de modo que a alternativa em texto “Empurre a tampa para baixo e gire no sentido anti-horário (da direita para a esquerda)” é fornecida no atributo alt.
+<section class="copy-code">
+                <button class="d-block ms-auto" aria-label="Copiar código">
+                    <img src="./img/copy.png" alt="duas paginas, umas sobrepondo a outra" title="copiar código">
+                </button>
+<div>
+<code>                   
+                    &lt;img src="./img/tampa.png"  class="img-fluid" alt="Empurre a tampa para baixo e gire no sentido anti-horário (da direita para a esquerda)"&gt;
+               
+            </code>
+            </div>
+            </section>
+<p>Exemplo 02: </p>
+<div class="row mt-5 mb-5" id="imgs_familia"> 
+<div class="col-12">
+                        <img src="./img/familia.jpg" class="img-fluid" alt="Somos amigos da família"id="img02"/>
+                    </div>
+                    </div>
+  <p>Esta fotografia mostra um grupo familiar feliz. Ele está sendo usado para dar a impressão de que o site ou a empresa que representa é familiar. A alternativa em texto é “Somos amigos da família”, pois é a que melhor descreve a impressão pretendida. </p>
+  <section class="copy-code">
+                <button class="d-block ms-auto" aria-label="Copiar código">
+                    <img src="./img/copy.png" alt="duas paginas, umas sobrepondo a outra" title="copiar código">
+                </button>
+  <div>
+  <code>   
+                    &lt;img src="./img/familia.jpg" class="img-fluid" alt="Somos amigos da família"&gt;
+               
+            </code>
+                </div>
+                </section>
+                <p> Grupos de imagens: se várias imagens transmitem uma única informação, a alternativa em texto para uma imagem deve transmitir as informações para todo o grupo.</p>
+  </div>
     </div>
+  </div>
   </div>
   <div class="col ">
     <div class="collapse multi-collapse" id="multiCollapseExample2">
       <div class="card card-body">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elit eros, pretium a elementum
-            vitae, scelerisque ac augue. Curabitur at euismod eros. Vestibulum diam libero, posuere non
-            luctus commodo, tincidunt nec lectus. Integer consectetur ipsum vel nunc fringilla, vel
-            pulvinar est ullamcorper. Proin in nibh et mi accumsan imperdiet. Pellentesque et libero
-            auctor, tempus arcu eget, venenatis massa. Phasellus eget lectus enim. Etiam dui lectus,
-            tincidunt at sem ac, semper dapibus sapien.
+<p>Imagens decorativas não adicionam informações ao conteúdo de uma página. Por exemplo, as informações fornecidas pela imagem já podem ser fornecidas em texto adjacente ou a imagem pode ser incluída para tornar o site mais atraente visualmente.
+
+Nesses casos, um texto alternativo nulo (vazio) deve ser fornecido (alt = "") para que possam ser ignorados por tecnologias assistivas, como leitores de tela. Os valores de texto para esses tipos de imagens adicionariam confusão audível à saída do leitor de tela ou poderiam distrair os usuários se o tópico fosse diferente do texto adjacente. Deixar de fora o atributo alt também não é uma opção porque, quando não é fornecido, alguns leitores de tela anunciarão o nome do arquivo da imagem.
+
+Tratar uma imagem como decorativa ou informativa é um julgamento que somente o autor pode fazer, com base no motivo da inclusão da imagem na página. As imagens podem ser decorativas quando:
+
+Estilo visual, como bordas, espaçadores e cantos;
+Suplementar para vincular o texto para melhorar sua aparência ou aumentar a área clicável;
+Ilustrativo do texto adjacente, mas não contribuindo com informações (“colírio para os olhos”);
+Identificado e descrito pelo texto circundante.</p>
+<div class="row mt-5 mb-5" id="imgs_decorativa"> 
+<div class="col-12">
+                        <img src="./img/barra.png" class="img-fluid" alt="imagem decorativa"id="img03"/>
+                    </div>
+                    </div>
+
+<section class="copy-code">
+                <button class="d-block ms-auto" aria-label="Copiar código">
+                    <img src="./img/copy.png" alt="duas paginas, umas sobrepondo a outra" title="copiar código">
+                </button>
+  <div>
+  <code>   
+                    &lt;img src="./img/barra.png" alt=""&gt;
+               
+            </code>
+                </div>
+                </section>
       </div>
     </div>
   </div>
   <div class="col ">
     <div class="collapse multi-collapse" id="multiCollapseExample3">
       <div class="card card-body">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elit eros, pretium a elementum
-            vitae, scelerisque ac augue. Curabitur at euismod eros. Vestibulum diam libero, posuere non
-            luctus commodo, tincidunt nec lectus. Integer consectetur ipsum vel nunc fringilla, vel
-            pulvinar est ullamcorper. Proin in nibh et mi accumsan imperdiet. Pellentesque et libero
-            auctor, tempus arcu eget, venenatis massa. Phasellus eget lectus enim. Etiam dui lectus,
-            tincidunt at sem ac, semper dapibus sapien.
-      </div>
+     <p>Imagens funcionais são usadas para iniciar ações em vez de transmitir informações. Eles são usados ​​em botões, links e outros elementos interativos. A alternativa em texto para a imagem deve transmitir a ação que será iniciada (o propósito da imagem), ao invés de uma descrição da imagem. Por exemplo, a alternativa em texto deve ser “imprimir esta página” em vez de “(imagem de uma) impressora”.</p>
+      <div class="row mt-5 mb-5" id="imgs_decorativa"> 
+<div class="col-12">
+                        <img src="./img/impressora.png" class="img-fluid" alt="impressora"id="img04"/>
+                    </div>
+                    </div>
+      <section class="copy-code">
+                <button class="d-block ms-auto" aria-label="Copiar código">
+                    <img src="./img/copy.png" alt="duas paginas, umas sobrepondo a outra" title="copiar código">
+                </button>
+  <div>
+  <code>   
+                    &lt;a href="javascript:print()"&gt;
+                    &lt;img src="impressora.png." alt="Imprimir esta página"&gt;&lt;/a&gt;
+            </code>
+                </div>
+                </section>
+      <p>Valores alt ausentes ou vazios criam problemas significativos para usuários de leitores de tela porque imagens funcionais são essenciais para a funcionalidade do conteúdo. Os leitores de tela geralmente anunciam o nome do arquivo de imagem, o URL da imagem ou o URL do destino do link, o que dificilmente ajudará os usuários a entender a ação que será iniciada pela imagem.</p>
     </div>
   </div>
 </div>
-        </article>
-    </main>
+<div class="col ">
+    <div class="collapse multi-collapse" id="multiCollapseExample4">
+      <div class="card card-body">
+      <p>Imagens complexas contêm informações substanciais - mais do que pode ser transmitido em uma frase ou frase curta. Normalmente são:
 
+gráficos e diagramas, incluindo fluxogramas e organogramas;
+diagramas e ilustrações em que o texto da página depende do usuário ser capaz de compreender a imagem;
+mapas mostrando locais ou outras informações, como sistemas meteorológicos.
+Nestes casos, é necessária uma alternativa em texto em duas partes. A primeira parte é a descrição curta para identificar a imagem e, quando apropriado, indicar a localização da descrição longa. A segunda parte é a longa descrição - uma representação textual das informações essenciais veiculadas pela imagem. 
+Lembre-se de que imagens complexas podem ser difíceis de entender por muitas pessoas - em particular pessoas com dificuldades de aprendizagem e pessoas com baixa visão. Descrições longas beneficiam muitas pessoas e é uma boa prática disponibilizá-las a todos, por exemplo, como parte do conteúdo principal. Também pode ser possível reduzir a complexidade desnecessária em suas imagens e torná-las mais fáceis de entender para todos.
+Também é uma boa prática referir-se e resumir imagens mais complexas do texto que acompanha. Por exemplo, uma referência como “O gráfico seguinte mostra que os visitantes se perderam no primeiro trimestre, mas os números recuperaram no segundo trimestre” ajuda a apontar as informações relevantes que a imagem pretende apresentar.
+Exemplo: um link de texto para a longa descrição adjacente à imagem.</p>
+<div class="row mt-5 mb-5" id="imgs_grafico"> 
+<div class="col-12">
+                        <img src="./img/grafico.png" class="img-fluid" alt="grafico"id="img05"/>
+                    </div>
+                    </div>
+      <section class="copy-code">
+                <button class="d-block ms-auto" aria-label="Copiar código">
+                    <img src="./img/copy.png" alt="duas paginas, umas sobrepondo a outra" title="copiar código">
+                </button>
+  <div>
+  <code>   
+                    &lt;p&gt;
+                    &lt;img
+		src="chart.png"
+		alt="Bar chart showing monthly and total visitors for the first quarter 2014 for sites 1 to 3"&gt;&lt;br&gt;
+                    &lt;a href="2014-first-qtr.html"&gt;&lt;Example.com Site visitors Jan to March 2014 text description of the bar chart&gt;/a&lt;/p&gt;
+                    &lt;/p&gt;
+            </code>
+                </div>
+                </section>
+                </div>
+  </div>
+</div>
+</article>
+    </main>
+                <aside>
+    <div class="row" id="Referência">
+            <p class="p1">
+                Referência Bibliográfica:
+                Web Accessibility Tutorials, 2021. Disponível
+                em: https://www.w3.org/WAI/tutorials/images/. Acesso em: 28 de
+                novembro de
+                2021.
+            </p>
+            </div>
+</aside>
+ 
     <?php include './includes/footer.php'; ?>
 </body>
 
--- tabelas.php
+++ tabelas.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <?php include './includes/bootstrap-css.php'; ?>
+    <link rel="stylesheet" href="./css/paginas/menu.css">
     <title>Tabelas | Seu site mais acessível</title>
 </head>
 
@@ -18,34 +19,94 @@
               </div>  
         </section>
         <article>   
-            <section>
-                <h2> Qual dessas tabelas abaixo você considera mais acessível visualmente falando?</h2>
-                <div class="row mt-5 mb-5" id="imgs_tabela">
-                    <div class="col-4">
-                        <img src="img/tabela1.jpg" class="img-fluid" alt="Quadrado">
-                    </div>
-                    <div class="col-4">
-                        <img src="img/tabela1.jpg" class="img-fluid" alt="Quadrado">
-                    </div>
-                    <div class="col-4">
-                        <img src="img/tabela1.jpg" class="img-fluid" alt="Quadrado ">
-                    </div>
+        <section> 
+        <div class="container text-center">
+                <h2> Tabela Acessível</h2>
                 </div>
+                <div class="row mt-5 mb-5 " id="imgs_tabela">
+                    <div class="col-12">
+                        <img src="./img/tabela02.png" class="img-fluid" alt="Tabela acessível">
+                    </div>
+                    </div>
             </section>
-            
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elit eros, pretium a elementum
-            vitae, scelerisque ac augue. Curabitur at euismod eros. Vestibulum diam libero, posuere non
-            luctus commodo, tincidunt nec lectus. Integer consectetur ipsum vel nunc fringilla, vel
-            pulvinar est ullamcorper. Proin in nibh et mi accumsan imperdiet. Pellentesque et libero
-            auctor, tempus arcu eget, venenatis massa. Phasellus eget lectus enim. Etiam dui lectus,
-            tincidunt at sem ac, semper dapibus sapien. Sed dolor quam, rutrum in vestibulum non,
-            ullamcorper quis sem. Integer ligula orci, convallis et placerat at, posuere nec dolor.
-            Pellentesque porta gravida tortor non eleifend. Aenean gravida nisi vel tempus rhoncus.
-            Proin orci justo, luctus vel nisl eget, porttitor vestibulum magna. Aliquam consectetur
-            fringilla varius. Aliquam sed fringilla sem.</p>
+            <section>
+            <div class="row" id="Texto tabela">
+            <p class="p1">
+            As tabelas de dados são usadas para organizar dados com um relacionamento lógico em grades. As tabelas acessíveis precisam de marcação HTML que indica células de cabeçalho e células de dados e define seu relacionamento. As tecnologias assistivas usam essas informações para fornecer contexto aos usuários.
 
-        </article>
+As células de cabeçalho devem ser marcadas com th e as células de dados com td para tornar as tabelas acessíveis. Para tabelas mais complexas, associações explícitas podem ser necessárias usando atributos de escopo, id e cabeçalhos.</p>
+Por que isso é importante?
+Tabelas sem marcação estrutural para diferenciar e vincular adequadamente entre o cabeçalho e as células de dados, criam barreiras de acessibilidade. Depender apenas de dicas visuais não é suficiente para criar uma mesa acessível. Com a marcação estrutural, cabeçalhos e células de dados podem ser determinados programaticamente por software, o que significa que:
+
+Pessoas que usam leitores de tela podem ter os cabeçalhos de linha e coluna lidos em voz alta enquanto navegam pela tabela. Os leitores de tela falam uma célula por vez e fazem referência às células de cabeçalho associadas, para que o leitor não perca o contexto.
+
+Algumas pessoas usam formas alternativas de renderizar os dados, por exemplo, usando folhas de estilo personalizadas para exibir células de cabeçalho com mais destaque. Técnicas como essa permitem que eles alterem o tamanho e as cores do texto e exibam as informações como listas em vez de grades. O código da tabela precisa ser estruturado adequadamente para permitir renderizações alternativas.
+A seguinte tabela de horários de abertura contém informações de cabeçalho na linha superior e na primeira coluna. Todas as células de cabeçalho são marcadas como th células com atributos de escopo adicionados.
+
+Na linha do cabeçalho, o valor col para o escopo associa cada célula do cabeçalho às células de dados na coluna. Na coluna do cabeçalho, o valor da linha associa os cabeçalhos individuais às suas linhas. Sem essas informações, alguns usuários não entenderiam facilmente a relação entre o cabeçalho e as células de dados. No exemplo abaixo, os rótulos “Aberto” e “Fechado” só são úteis se puderem ser atribuídos a um determinado dia e hora.
+</p>
+<div class="row mt-5 mb-5" id="imgs_tabela"> 
+<div class="col-12">
+                        <img src="./img/tabela01.png" class="img-fluid" alt="tabela">
+                    </div>
+                    </div>
+                    </section>
+<section class="copy-code">
+                <button class="d-block ms-auto" aria-label="Copiar código">
+                    <img src="./img/copy.png" alt="duas paginas, umas sobrepondo a outra" title="copiar código">
+                </button>
+  <div>
+  <code>   
+  &lt;table&gt;
+                    &lt;caption&gt; &lt;Delivery slots:&gt;&lt;/caption&gt;
+                    &lt;tr&gt;
+                    &lt;td&gt;/td&lt;
+                    &lt;th scope="col">Monday&gt;/th&lt;
+                    &lt;th scope="col">Tuesday&gt;/th&lt;
+                    &lt;th scope="col">Wednesday&gt;/th&lt;
+                    &lt;th scope="col">Thursday&gt;/th&lt;
+                    &lt;th scope="col">Friday&gt;/th&lt;
+                    &lt;/tr&gt;
+                    &lt;tr&gt;
+                    &lt;th scope="row"&gt;09:00 - 11:00&lt;&lt; &lt;/th&gt;
+                    &lt;td&gt; &lt;Closed&gt;/td&lt;
+                    &lt;td&gt; &lt;Open&gt;/td&lt;
+                    &lt;td&gt; &lt;Open&gt;/td&lt;
+                    &lt;td&gt; &lt;Closed&gt;/td&lt;
+                    &lt;td&gt; &lt;Closed&gt;/td&lt;
+                    &lt;/tr&gt;
+                    &lt;tr&gt;
+                    &lt;th scope="row"&gt;11:00 - 13:00&lt;
+                    &lt;td&gt; &lt;Open&gt;/td&lt;
+                    &lt;td&gt; &lt;Open&gt;/td&lt;
+                    &lt;td&gt; &lt;Closed&gt;/td&lt;
+                    &lt;td&gt; &lt;Closed&gt;/td&lt;
+                    &lt;td&gt; &lt;Closed&gt;/td&lt;
+                    &lt;/tr&gt;
+                    […]
+                    &lt;/table&gt;
+            </code>
+                </div>
+                </section>
+</article>
     </main>
+    <aside>
+    <div class="row" id="Referência">
+            <p class="p1">
+            Referência Bibliográfica:
+                Web Accessibility Tutorials, 2021. Disponível
+                em: https://www.w3.org/WAI/tutorials/tables/. Acesso em: 28 de
+                novembro de
+                2021.
+                </p>
+                <p class="p2">
+                Tabelas Acessíveis , 2021. Disponível
+                em:https://www.gov.br/governodigital/pt-br/acessibilidade-digital/tabelas-acessiveis.pdf. Acesso em: 28 de
+                novembro de
+                2021.
+            </p>
+            </div>
+</aside>
     <?php include './includes/footer.php'; ?>
 </body>
 </html>
