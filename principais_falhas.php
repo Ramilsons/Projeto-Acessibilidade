c24deb366fc581bec20b80e4b9ccf7ec5206644b
VitorHugoTeixeira <vitor4oliveira@gmail.com>
2021-12-01 22:58:30 -0300

    Página principais_falhas preparada para apresentação

--- principais_falhas.php
+++ principais_falhas.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <?php include './includes/bootstrap-css.php'; ?>
+    <link rel="stylesheet" href="css/paginas/principais.css">
     <title>Principais Falhas | Seu site mais acessível</title>
 </head>
 
@@ -14,25 +15,31 @@
             <div class="container">
                 <div class="row">
                     <div class="col-6">
-                        <img src="" alt="..." class="img-fluid">
+                        <img src="img/principais falhas/principais1.jpg" alt="..." class="img-fluid">
                     </div>
-                    <div class="col-6">
-                        <h1>Principais Falhas</h1>
-                        <h3>--Data correspondente--</h3>
+                    <div class="col-6 titulo">
+                        <h1>Principais</h1>
+                        <h1>Falhas</h1>
+                        <!-- <h2>Data Correspondente</h2> -->
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="col-6">
-                        <h2>Imagens acessíveis</h2>
-                        <p>As imagens são acessadas por pessoas com deficiência visual (cegas ou de baixa visão) através de leitores de tela que identificam os textos alternativos e retornam o conteúdo em forma de áudio para o usuário. Se esses textos não existem ou estão incompreensíveis, o usuário pode não entender o contexto ou o significado daquela imagem.</p>
-                    </div>
-                    <hr>
-                    <div class="col-6">
-                        <h2>Navegação por teclado</h2>
-                        <p>A mais importante ferramenta para a navegação é permitir que o usuário consiga identificar onde está o foco da ação. Ao identificar o foco, ele consegue saber quais são os itens interativos da página e navegar normalmente. Se esse foco não está visível, o usuário se perde na navegação. O ideal é que o foco esteja visível não somente quando se passa o mouse, mas quando se faz o foco por teclado. </p>
-                    </div>
+
+                <div class="row conteudo">
+                    <h2>1. Imagens acessíveis</h2>
+                    <p>As imagens são acessadas por pessoas com deficiência visual (cegas ou de baixa visão) através de leitores de tela que identificam os textos alternativos e retornam o conteúdo em forma de áudio para o usuário. Se esses textos não existem ou estão incompreensíveis, o usuário pode não entender o contexto ou o significado daquela imagem.</p>
                 </div>
+
+                <div class="row linha">
+                    <hr size="4">
+                </div>
+                
+  
+                <div class="row conteudo">
+                    <h2>2. Navegação por teclado</h2>
+                    <p>A mais importante ferramenta para a navegação é permitir que o usuário consiga identificar onde está o foco da ação. Ao identificar o foco, ele consegue saber quais são os itens interativos da página e navegar normalmente. Se esse foco não está visível, o usuário se perde na navegação. O ideal é que o foco esteja visível não somente quando se passa o mouse, mas quando se faz o foco por teclado. </p>
+                </div>
+
             </div>
         </article>
     </main>
