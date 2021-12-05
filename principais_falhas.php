62ddd43305a3fd3eaf65773407b32467a49057d6
Vitor <vitor4oliveira@gmail.com>
2021-12-05 12:03:27 -0300

    Adicionando mais conteudo a página principaisFalhas"

--- principais_falhas.php
+++ principais_falhas.php
@@ -12,7 +12,7 @@
     <?php include './includes/header.php'; ?>
     <main>
         <article>
-            <div class="container">
+            <div class="container" id="principal">
                 <div class="row">
                     <div class="col-6">
                         <img src="img/principais falhas/principais1.jpg" alt="Imagem de um garoto debochando de outro garoto em um ambiente escolar. Imagem tirada do site freepik.com" class="img-fluid">
@@ -32,12 +32,32 @@
                     <hr size="4">
                 </div>
                 
-  
                 <div class="row conteudo">
                     <h2>2. Navegação por teclado</h2>
                     <p>A mais importante ferramenta para a navegação é permitir que o usuário consiga identificar onde está o foco da ação. Ao identificar o foco, ele consegue saber quais são os itens interativos da página e navegar normalmente. Se esse foco não está visível, o usuário se perde na navegação. O ideal é que o foco esteja visível não somente quando se passa o mouse, mas quando se faz o foco por teclado. </p>
                 </div>
 
+                <div class="row linha">
+                    <hr size="4">
+                </div>
+
+                <div class="row conteudo">
+                    <h2>3. Descrição de Links</h2>
+                    <p>É possivel observar que boa parte dos links presentes em sites pelo nosso país contém uma descrição curta, sem nenhuma indicação de onde está enviando o usuário. O correto nessas situações é sempre optar por uma descrição mais completa mostrando ao usuário para onde ele será enviado ao clicar naquele link.</p>
+                </div>
+
+                <div class="row linha">
+                    <hr size="4">
+                </div>
+
+                <div class="row conteudo">
+                    <h2>3. Formulários</h2>
+                    <p>Os formulários são em sua essência formas de interagir e coletar informações do usuário. Eles obviamente devem ser acessíveis a qualquer pessoa que acesse sua aplicação, por isso deve se atentar as descrições dos campos do formulário e também o tipo de navegação que pode se utilizar para andar pelo campos do formulário. Muitas vezes os campos não possuem a descrição correta dificultando a compreensão de pessoas que possuem deficiência</p>
+                </div>
+
+                <div class="row">
+
+                </div>
             </div>
         </article>
     </main>
